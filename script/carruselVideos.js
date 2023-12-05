48e7186612f5572489e94ff0871d2418773b2743
JonaArro <arroyo.jonatan@gmail.com>
2023-12-05 20:08:37 -0300

    Mejora en la logica del carrusel de Page Video agregando la variable currentIframe para evaluar la reproduccion si se va a un video anterior o posterior; creacion de archivo SCSS y compilación; bajada de extensión Prettier Code Form y aplicación

--- script/carruselVideos.js
+++ script/carruselVideos.js
@@ -1,11 +1,13 @@
 const videoContainer = document.querySelector('.videoContainer');
 const videoSlides = document.querySelectorAll('.videoSlide');
 let currentIndex = 0;
+let currentIframe;
 
 function showVideo(index) {
     videoSlides.forEach((slide, i) => {
         if (i === index) {
             slide.style.display = 'block';
+            currentIframe = slide.querySelector('iframe');
         } else {
             slide.style.display = 'none';
         }
@@ -13,19 +15,29 @@ function showVideo(index) {
 }
 
 function nextVideo() {
+    if (currentIframe) {
+        // Detener la reproducción del video actual antes de pasar al siguiente
+        currentIframe.src = currentIframe.src; // Reemplazar el iframe para detener la reproducción
+    }
+
     currentIndex = (currentIndex + 1) % videoSlides.length;
     showVideo(currentIndex);
 }
 
 function prevVideo() {
+    if (currentIframe) {
+        // Detener la reproducción del video actual antes de retroceder al anterior
+        currentIframe.src = currentIframe.src; // Reemplazar el iframe para detener la reproducción
+    }
+
     currentIndex = (currentIndex - 1 + videoSlides.length) % videoSlides.length;
     showVideo(currentIndex);
 }
 
-// muestro el primer video al cargar la página
+// Mostrar el primer video al cargar la página
 showVideo(currentIndex);
 
-// agrego botones para navegar por el carrusel
+// Agregar botones para navegar por el carrusel
 const prevButton = document.createElement('button');
 prevButton.textContent = 'Anterior';
 prevButton.classList.add('botonCarruselAnterior');
@@ -37,4 +49,4 @@ nextButton.classList.add('botonCarruselSiguiente');
 nextButton.addEventListener('click', nextVideo);
 
 videoContainer.appendChild(prevButton);
-videoContainer.appendChild(nextButton); 
+videoContainer.appendChild(nextButton);
